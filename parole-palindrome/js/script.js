226d7bf09d2ef5af6cb0716af24059b002beacee
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-22 17:26:13 +0200

    creato la condizione

--- parole-palindrome/js/script.js
+++ parole-palindrome/js/script.js
@@ -4,19 +4,29 @@
 
 // Creo una costante per chiedere all'utente di inserire una parola
 const userWord = prompt('Inserisci una parola');
-let firstString = 'ciao';
 
+// Creo una funzione per la parola contraria
 function reverse(firstString) {
 
+    // Creo una variabile per la parola contraria
     let result = '';
 
+    // Creo un ciclo per verificare la parola contraria
     for (let i = firstString.length - 1; i >= 0; i--) {
         result += firstString[i];
+
+        // Se la parola contraria è uguale alla parola originaria, allora la parola è palindroma
+    } if (result === firstString) {
+        console.log(`${firstString} è palindroma`);
+
+        // Altrimenti la parola non è palindroma
+    } else {
+        console.log(`${firstString} non è palindroma`);
     }
 
+    // Restituisco la parola contraria
     return result;
 
 }
 
 console.log(reverse(userWord));
-console.log(reverse(firstString));
