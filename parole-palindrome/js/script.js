f32334e70c9f9cb5171228ef04b8663bac61afbe
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-22 15:19:16 +0200

    first commit

---
+++ parole-palindrome/js/script.js
@@ -0,0 +1,17 @@
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+
+// Creo una costante per chiedere all'utente di inserire una parola
+const userWord = prompt('Inserisci una parola');
+
+
+// Chiamo la funzione per capire se la parola inserita è palindroma
+function isPalindrome(word) {
+
+
+
+}
+
+// Stampo in console la risposta della funzione
+isPalindrome(userWord);
